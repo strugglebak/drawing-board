e91d483953638cf532383c533f2cd60a03d3170b
xm <dxp157@sohu.com>
2018-06-05 07:37:02 +0800

    Add eraser function of Canvas

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 
 var canvas = document.getElementById('canvas');
+var eraserButton = document.getElementById('eraser');
+
 var ctx = canvas.getContext('2d');
 
 var lineWidth = 20;
 var isPainting = false;
+var isErasing = false;
 var lastPosition = {'x': undefined, 'y': undefined};
-
 
 // 画圈
 function drawCircle(positionX, positionY, radius) {
@@ -25,6 +27,11 @@ function drawLine(positionX1, positionY1, positionX2, positionY2) {
   ctx.closePath();
 }
 
+// 擦除
+function erasePatten(positionX, positionY) {
+  ctx.clearRect(positionX, positionY, 10, 10);
+}
+
 // 放大和缩小窗口
 function windowResize(canvasDiv) {
   var curPageWidth = document.documentElement.clientWidth;
@@ -36,38 +43,43 @@ function windowResize(canvasDiv) {
 
 // 按下鼠标
 canvas.onmousedown = function(args) {
-  isPainting = true;
-
   var x = args.clientX;
   var y = args.clientY;
-
   lastPosition = {'x': x, 'y': y}
-
-  drawCircle(x, y, 2.5);
+  if (isErasing) {
+    erasePatten(lastPosition.x, lastPosition.y);
+  } else {
+    isPainting = true;
+    drawCircle(x, y, 2.5);
+  }
 }
 
 // 动鼠标
 canvas.onmousemove = function(args) {
-
-  if (isPainting !== true) {
-    return;
-  }
-
   var x = args.clientX;
   var y = args.clientY;
   var nextPosition = {'x': x, 'y': y}
-
-  drawLine(lastPosition.x, lastPosition.y, nextPosition.x, nextPosition.y);
-
+  if (isErasing) {
+    erasePatten(lastPosition.x, lastPosition.y);
+  } else {
+    if (isPainting) {
+      drawLine(lastPosition.x, lastPosition.y, nextPosition.x, nextPosition.y);
+    }
+  }
   lastPosition = nextPosition;
 }
 
 // 松开鼠标
 canvas.onmouseup = function(args) {
   isPainting = false;
+  isErasing = false;
 }
 
 // 窗口缩放
 window.onresize = function() {
   windowResize(canvas);
+}
+
+eraserButton.onclick = function() {
+  isErasing = true;
 }
