2d7180cc2d178c7424e9dc8f4ea93dc0251be41f
xm <dxp157@sohu.com>
2018-06-05 20:35:05 +0800

    Add brush and eraser function into canvas and compelete the basic function of Canvas board

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 
 var canvas = document.getElementById('canvas');
 var eraserButton = document.getElementById('eraser');
+var brushButton = document.getElementById('brush');
+var actions = document.getElementById('actions');
 
 var ctx = canvas.getContext('2d');
 
 var lineWidth = 20;
-var isPainting = false;
-var isErasing = false;
+var isUsing = false;
+var eraseEnabled = false;
 var lastPosition = {'x': undefined, 'y': undefined};
+
+windowResize(canvas);
 
 // 画圈
 function drawCircle(positionX, positionY, radius) {
@@ -29,7 +33,7 @@ function drawLine(positionX1, positionY1, positionX2, positionY2) {
 
 // 擦除
 function erasePatten(positionX, positionY) {
-  ctx.clearRect(positionX, positionY, 10, 10);
+  ctx.clearRect(positionX, positionY, 50, 50);
 }
 
 // 放大和缩小窗口
@@ -43,36 +47,40 @@ function windowResize(canvasDiv) {
 
 // 按下鼠标
 canvas.onmousedown = function(args) {
+  isUsing = true;
   var x = args.clientX;
   var y = args.clientY;
-  lastPosition = {'x': x, 'y': y}
-  if (isErasing) {
-    erasePatten(lastPosition.x, lastPosition.y);
+
+  if (eraseEnabled) {
+    erasePatten(x, y);
   } else {
-    isPainting = true;
     drawCircle(x, y, 2.5);
+    lastPosition = {'x': x, 'y': y}
   }
 }
 
 // 动鼠标
 canvas.onmousemove = function(args) {
+  if (!isUsing) {
+    return;
+  }
+
   var x = args.clientX;
   var y = args.clientY;
   var nextPosition = {'x': x, 'y': y}
-  if (isErasing) {
+
+  if (eraseEnabled) {
     erasePatten(lastPosition.x, lastPosition.y);
   } else {
-    if (isPainting) {
-      drawLine(lastPosition.x, lastPosition.y, nextPosition.x, nextPosition.y);
-    }
+    drawLine(lastPosition.x, lastPosition.y, nextPosition.x, nextPosition.y);
   }
+
   lastPosition = nextPosition;
 }
 
 // 松开鼠标
 canvas.onmouseup = function(args) {
-  isPainting = false;
-  isErasing = false;
+  isUsing = false;
 }
 
 // 窗口缩放
@@ -81,5 +89,10 @@ window.onresize = function() {
 }
 
 eraserButton.onclick = function() {
-  isErasing = true;
+  eraseEnabled = true;
+  actions.className = 'actions display';
+}
+brushButton.onclick = function() {
+  eraseEnabled = false;
+  actions.className = 'actions';
 }
